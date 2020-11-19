c89f8dfb80f3a1ebb42f639a931587dbedd71791
margaritasabaseva <m.sabaseva@gmail.com>
2020-11-19 23:41:00 +0200

    Edited user registration page

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,43 +1,47 @@
-<x-guest-layout>
-    <x-jet-authentication-card>
-        <x-slot name="logo">
-            <x-jet-authentication-card-logo />
-        </x-slot>
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Reģistrēt jaunu lietotāju') }}
+        </h2>
+    </x-slot>
 
-        <x-jet-validation-errors class="mb-4" />
+    <x-jet-validation-errors class="mb-4" />
 
-        <form method="POST" action="{{ route('register') }}">
-            @csrf
+        <div>
+            <div class="max-w-4xl mx-auto py-10 sm:px-6 lg:px-20">
+                <form method="POST" action="{{ route('register') }}">
+                    @csrf
 
-            <div>
-                <x-jet-label for="name" value="{{ __('Vārds, uzvārds') }}" />
-                <x-jet-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
+                    <div>
+                        <x-jet-label for="name" value="{{ __('Vārds, uzvārds') }}" />
+                        <x-jet-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
+                    </div>
+
+                    <div>
+                        <x-jet-label for="email" value="{{ __('E-pasts') }}" />
+                        <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required />
+                    </div>
+
+                    <div>
+                        <x-jet-label for="password" value="{{ __('Parole') }}" />
+                        <x-jet-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="new-password" />
+                    </div>
+
+                    <div>
+                        <x-jet-label for="password_confirmation" value="{{ __('Apstiprināt paroli') }}" />
+                        <x-jet-input id="password_confirmation" class="block mt-1 w-full" type="password" name="password_confirmation" required autocomplete="new-password" />
+                    </div>
+
+                    <div class="flex items-center justify-end mt-4">
+                        <!-- <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('login') }}">
+                            {{ __('Jau esat reģistrējies?') }}
+                        </a> -->
+
+                        <x-jet-button class="ml-4">
+                            {{ __('Reģistrēt jaunu lietotāju') }}
+                        </x-jet-button>
+                    </div>
+                </form>
             </div>
-
-            <div class="mt-4">
-                <x-jet-label for="email" value="{{ __('E-pasts') }}" />
-                <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required />
-            </div>
-
-            <div class="mt-4">
-                <x-jet-label for="password" value="{{ __('Parole') }}" />
-                <x-jet-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="new-password" />
-            </div>
-
-            <div class="mt-4">
-                <x-jet-label for="password_confirmation" value="{{ __('Apstiprināt paroli') }}" />
-                <x-jet-input id="password_confirmation" class="block mt-1 w-full" type="password" name="password_confirmation" required autocomplete="new-password" />
-            </div>
-
-            <div class="flex items-center justify-end mt-4">
-                <!-- <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('login') }}">
-                    {{ __('Jau esat reģistrējies?') }}
-                </a> -->
-
-                <x-jet-button class="ml-4">
-                    {{ __('Reģistrēt jaunu lietotāju') }}
-                </x-jet-button>
-            </div>
-        </form>
-    </x-jet-authentication-card>
-</x-guest-layout>
+        </div>    
+</x-app-layout>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,3 +1,5 @@
+<!-- Netiek nekur izmantots kā frontend blade -->
+
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
@@ -25,7 +27,7 @@
             @if (Route::has('login'))
                 <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
                     @auth
-                        <a href="{{ url('/dashboard') }}" class="text-sm text-gray-700 underline">Dashboard</a>
+                        <a href="{{ url('/working-hours') }}" class="text-sm text-gray-700 underline">Dashboard</a>
                     @else
                         <a href="{{ route('login') }}" class="text-sm text-gray-700 underline">Login</a>
 
