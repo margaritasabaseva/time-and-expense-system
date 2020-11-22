1d6261bd4e889a6e1aafb616b255c593d5a28d01
margaritasabaseva <m.sabaseva@gmail.com>
2020-11-22 23:52:38 +0200

    Added controller specific routes

--- routes/web.php
+++ routes/web.php
@@ -17,26 +17,32 @@ Route::get('/', function () {
     return view('/auth/login');
 });
 
-Route::middleware(['auth:sanctum', 'verified'])->get('/projects', function () {
-    return view('projects');
-})->name('projects');
+// Route::middleware(['auth:sanctum', 'verified'])->get('/employee/working-hours', function () {
+//     return view('employee/working-hours');
+// })->name('working-hours');
 
-Route::middleware(['auth:sanctum', 'verified'])->get('/working-hours', function () {
-    return view('working-hours');
-})->name('working-hours');
-
-Route::middleware(['auth:sanctum', 'verified'])->get('/expense-report', function () {
-    return view('personal-expense-report');
+Route::middleware(['auth:sanctum', 'verified'])->get('/employee/expense-report', function () {
+    return view('employee/personal-expense-report');
 })->name('personal-expense-report');
 
-Route::middleware(['auth:sanctum', 'verified'])->get('/time-report', function () {
-    return view('time-report');
+Route::middleware(['auth:sanctum', 'verified'])->get('/manager/time-report', function () {
+    return view('manager/time-report');
 })->name('time-report');
 
-Route::middleware(['auth:sanctum', 'verified'])->get('/all-users', function () {
-    return view('all-users');
+// Route::middleware(['auth:sanctum', 'verified'])->get('/manager/projects', function () {
+//     return view('manager/projects');
+// })->name('projects');
+
+Route::middleware(['auth:sanctum', 'verified'])->get('/admin/all-users', function () {
+    return view('admin/all-users');
 })->name('all-users');
 
-Route::middleware(['auth:sanctum', 'verified'])->get('/auth/register', function () {
-    return view('auth/register');
-})->name('register');
+// Route::middleware(['auth:sanctum', 'verified'])->get('/admin/register', function () {
+//     return view('admin/register');
+// })->name('register');
+
+Route::get('/employee', 'EmployeeController@index');
+
+Route::get('/manager', 'ManagerController@index');
+
+Route::get('/admin', 'AdminController@index');
