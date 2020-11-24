f6863a3ceaf9b950a65a9e980dd2974c7c7ed5d9
margaritasabaseva <m.sabaseva@gmail.com>
2020-11-25 00:42:56 +0200

    Added methods to check roles

--- app/Models/User.php
+++ app/Models/User.php
@@ -69,4 +69,38 @@ class User extends Authenticatable
             ->belongsToMany(Role::class)
             ->withTimestamps();
     }
+
+    // to check against multiple roles by passing an array 
+    public function authorizeRoles($roles)
+    {
+        if ($this->hasAnyRole($roles)) {
+            return true;
+        }
+        abort(401, 'This action is unauthorized.');
+    }
+
+    public function hasAnyRole($roles)
+    {
+        if (is_array($roles)) {
+            foreach ($roles as $role) {
+                if ($this->hasRole($role)) {
+                    return true;
+                }
+            }
+        } else {
+            if ($this->hasRole($roles)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // to check just against a single role
+    public function hasRole($role)
+    {
+        if ($this->roles()->where(‘name’, $role)->first()) {
+            return true;
+        }
+        return false;
+    }
 }
