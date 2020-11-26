fb9f414fb3f74588aeb481dc7e3008e98a2999e6
margaritasabaseva <m.sabaseva@gmail.com>
2020-11-26 23:56:04 +0200

    Registered admin and manager middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -63,5 +63,7 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'role' => \App\Http\Middleware\CheckRole::class,
+        'admin' => \App\Http\Middleware\CheckAdmin::class,
+        'manager' => \App\Http\Middleware\CheckManager::class,
     ];
 }
