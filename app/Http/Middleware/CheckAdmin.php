9c991784dcf0c9a8a9dae09222589bceed9a7fce
margaritasabaseva <m.sabaseva@gmail.com>
2020-11-26 23:54:24 +0200

    Added middleware to check admin and manager roles

---
+++ app/Http/Middleware/CheckAdmin.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class CheckAdmin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        $userRoles = Auth::user()->roles->pluck('name');
+
+        if (!$userRoles->contains('ROLE_ADMIN')) {
+            return redirect('/home');
+        }
+
+        return $next($request);
+    }
+}
