28d20ed2fd5a2431905c0d900af8515190212625
margaritasabaseva <m.sabaseva@gmail.com>
2020-11-27 10:11:12 +0200

    Added basic view of all users

--- resources/views/admin/all-users.blade.php
+++ resources/views/admin/all-users.blade.php
@@ -7,9 +7,32 @@
 
     <x-jet-validation-errors class="mb-4" />
 
-        <div>
-            <div class="max-w-4xl mx-auto py-10 sm:px-6 lg:px-8">
-
+    <div>
+        <div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
+            <div class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
+                <div class="grid grid-cols-1 md:grid-cols-1">
+                    @foreach ($users as $user)
+                        <div class="p-6">                
+                            <div class="flex items-center">
+                                <div class="ml-4 pl-8 text-lg leading-7 font-semibold">{{ $user->name }}</div>
+                            </div>
+                            <div class="ml-12">
+                                <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
+                                    Amats: {{ $user->jobTitle }} <br>
+                                    E-pasta adrese: {{ $user->email }} <br>
+                                    Tālruņa numurs: {{ $user->phone }} <br>
+                                    @foreach ($user->roles as $role)
+                                        Lietotāja lomas: {{ $role->name }}
+                                    @endforeach
+                                    <div class="pt-10">
+                                        <x-jet-section-border />
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    @endforeach
+                </div>
             </div>
-        </div>    
+        </div>
+    </div>   
 </x-app-layout>
