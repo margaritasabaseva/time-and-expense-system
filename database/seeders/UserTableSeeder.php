0b99f5174f52520dba2b9a974f0a4de6cea8efc3
margaritasabaseva <m.sabaseva@gmail.com>
2020-11-27 10:06:00 +0200

    Edited user table seeder data

--- database/seeders/UserTableSeeder.php
+++ database/seeders/UserTableSeeder.php
@@ -19,21 +19,27 @@ class UserTableSeeder extends Seeder
         DB::table('users')->insert([
             [
                 'id' => '3',
-                'name' => 'Parasts Darbinieks Jānis',
+                'name' => 'Parasts Darbinieks',
                 'email' => 'employee@email.com',
-                'password' => Hash::make('password')
+                'password' => Hash::make('lotisarezgitaparole'),
+                'jobTitle' => 'Personāldaļas speciālists',
+                'phone' => '+371 22334455'
             ],
             [
                 'id' => '2',
-                'name' => 'Projektu Vadītāja Līga',
+                'name' => 'Projektu Vadītājs',
                 'email' => 'manager@email.com',
-                'password' => Hash::make('password')
+                'password' => Hash::make('lotisarezgitaparole'),
+                'jobTitle' => 'Pārdošanas nodaļas vecākais projektu vadītājs',
+                'phone' => '+371 29988776'
             ],
             [
                 'id' => '1',
-                'name' => 'Administrators Anna',
+                'name' => 'Administrators',
                 'email' => 'admin@email.com',
-                'password' => Hash::make('password')
+                'password' => Hash::make('lotisarezgitaparole'),
+                'jobTitle' => 'IT sistēmas administrators',
+                'phone' => '+371 29988776'
             ]
         ]);
     }
