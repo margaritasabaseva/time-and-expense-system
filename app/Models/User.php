0b5a00e96109b7509ce8fd0418de235410985941
margaritasabaseva <m.sabaseva@gmail.com>
2020-11-29 23:21:06 +0200

    Deleted unused methods

--- app/Models/User.php
+++ app/Models/User.php
@@ -80,44 +80,4 @@ class User extends Authenticatable
         }
         return false;
     }
-
-
-    // to check against multiple roles by passing an array 
-    // public function authorizeRoles($roles)
-    // {
-    //     if ($this->hasAnyRole($roles)) {
-    //         return true;
-    //     }
-    //     abort(401, 'This action is unauthorized.');
-    // }
-
-
-    // public function hasAnyRole($roles)
-    // {
-    //     if (is_array($roles)) {
-    //         foreach ($roles as $role) {
-    //             if ($this->hasRole($role)) {
-    //                 return true;
-    //             }
-    //         }
-    //     } else {
-    //         if ($this->hasRole($roles)) {
-    //             return true;
-    //         }
-    //     }
-    //     return false;
-    // }
-
-
-    // to check just against a single role
-    // public function hasRole($roles)
-    // {
-    //     foreach ($roles as $role) {
-    //         if ($this->roles->contains('name', $role)) {
-    //     // if ($this->roles()->where('name', $role)->first()) {
-    //         return true;
-    //         }
-    //     }
-    //     return false;
-    // }
 }
