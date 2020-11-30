6accee7d49a0da4bdf4219c35299344b20f178af
margaritasabaseva <m.sabaseva@gmail.com>
2020-11-30 23:53:57 +0200

    Changed role select dropdown for checkboxes

--- resources/views/admin/register.blade.php
+++ resources/views/admin/register.blade.php
@@ -21,33 +21,42 @@
                     </div>
                     <div>
                         <x-jet-label for="name" value="{{ __('Vārds, uzvārds') }}" />
-                        <x-jet-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
+                        <x-jet-input id="name" class="block mt-1 mb-3 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
                     </div>
 
                     <div>
                         <x-jet-label for="email" value="{{ __('E-pasts') }}" />
-                        <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required />
+                        <x-jet-input id="email" class="block mt-1 mb-3 w-full" type="email" name="email" :value="old('email')" required />
                     </div>
 
                     <div>
-                        <x-jet-label for="email" value="{{ __('Lietotāja grupa') }}" />
-                        <!-- Add arrow for this dropdown! Right now using form-input class from public\css, need to add class with arrow in resources\css -->
-                        <!-- Need to save chosen option as key to user table -->
-                        <select class="arrow block mt-1 w-full form-input rounded-md shadow-smv">
-                            <option>Pamatlietotājs</option>
-                            <option>Projektu vadītājs/ Grāmatvedis</option>
-                            <option>Administrators</option>
-                        </select>
+                        <x-jet-label for="role" value="{{ __('Lietotāja lomas') }}" />
+
+                        <label for="ROLE_EMPLOYEE" class="flex items-center mt-1">
+                            <input id="ROLE_EMPLOYEE" type="checkbox" class="form-checkbox" name="ROLE_EMPLOYEE">
+                            <span class="ml-2 text-sm text-gray-600">{{ __('Pamatlietotājs') }}</span>
+                        </label>
+                        
+                        <label for="ROLE_MANAGER" class="flex items-center">
+                            <input id="ROLE_MANAGER" type="checkbox" class="form-checkbox" name="ROLE_MANAGER">
+                            <span class="ml-2 text-sm text-gray-600">{{ __('Projektu vadītājs/ Grāmatvedis') }}</span>
+                        </label>
+
+                        <label for="ROLE_ADMIN" class="flex items-center mb-3">
+                            <input id="ROLE_ADMIN" type="checkbox" class="form-checkbox" name="ROLE_ADMIN">
+                            <span class="ml-2 text-sm text-gray-600">{{ __('Administrators') }}</span>
+                        </label>
+                        
                     </div>
 
                     <div>
                         <x-jet-label for="password" value="{{ __('Parole') }}" />
-                        <x-jet-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="new-password" />
+                        <x-jet-input id="password" class="block mt-1 mb-3 w-full" type="password" name="password" required autocomplete="new-password" />
                     </div>
 
                     <div>
                         <x-jet-label for="password_confirmation" value="{{ __('Apstiprināt paroli') }}" />
-                        <x-jet-input id="password_confirmation" class="block mt-1 w-full" type="password" name="password_confirmation" required autocomplete="new-password" />
+                        <x-jet-input id="password_confirmation" class="block mt-1 mb-3 w-full" type="password" name="password_confirmation" required autocomplete="new-password" />
                     </div>
 
                     <div class="flex items-center justify-end mt-4">
