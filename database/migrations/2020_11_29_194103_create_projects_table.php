a0b76e58e519c7ce86ef5b5bbc6b029d1db64dd0
margaritasabaseva <m.sabaseva@gmail.com>
2020-11-30 23:53:09 +0200

    Changed name to title

--- database/migrations/2020_11_29_194103_create_projects_table.php
+++ database/migrations/2020_11_29_194103_create_projects_table.php
@@ -15,7 +15,7 @@ class CreateProjectsTable extends Migration
     {
         Schema::create('projects', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
+            $table->string('title');
             $table->string('description');
             $table->string('startDate');
             // $table->unsignedInteger('user_id');
