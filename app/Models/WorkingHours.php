56b59c99955ea9cc98013f96e01ca9313f6f6e1f
margaritasabaseva <m.sabaseva@gmail.com>
2020-12-13 23:43:25 +0200

    Added table for working hours

---
+++ app/Models/WorkingHours.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class WorkingHours extends Model
+{
+    use HasFactory;
+
+    protected $casts = [
+        'working-hours' => 'array'
+    ];
+}
---
+++ database/migrations/2020_12_13_151926_create_working_hours_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateWorkingHoursTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('working_hours', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+
+            $table->id();
+            $table->foreign('project_id')->references('id')->on('projects')->onCascade('delete');
+            $table->foreign('user_id')->references('id')->on('users')->onCascade('delete');
+            $table->json('working_hours');
+            $table->integer('month');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('working_hours');
+    }
+}
