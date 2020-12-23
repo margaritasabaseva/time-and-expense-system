5b90ab4fdd2ecf8f99c133239dbc5a53b654948a
margaritasabaseva <m.sabaseva@gmail.com>
2020-12-23 23:55:55 +0200

    Finished with localization/translations

---
+++ resources/lang/lv/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'reset' => 'Jūsu parole ir nomainīta!',
+    'sent' => 'Mēs esam nosūtījuši Jūsu paroles atiestatīšanas saiti uz Jūsu e-pasta adresi!',
+    'throttled' => 'Lūdzu, uzgaidiet, pirms mēģināt vēlreiz.',
+    'token' => 'Šis paroles atiestatīšanas marķieris nav derīgs.',
+    'user' => "Mēs nevaram atrast lietotāju ar šādu e-pasta adresi.",
+
+];
